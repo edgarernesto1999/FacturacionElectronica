25ea562d93516cd6af88c455fa6404301e52fa39
Maybelline Navarro <maybenavarro20@gmail.com>
2025-11-08 21:29:08 -0500

    Gestion productos

--- FacturacionElectronica.Api/DTOs/ProductDtos.cs
+++ FacturacionElectronica.Api/DTOs/ProductDtos.cs
@@ -1,17 +1,20 @@
 namespace FacturacionElectronica.Api.DTOs
 {
+  //DTO para crear un nuevo producto
   public record ProductCreateDto(
     string TipoProducto,
     String Nombre,
     bool Activo = true
     );
 
+//DTO para actualizar un producto existente
   public record ProductUpdateDto(
     string TipoProducto,
     string Nombre,
     bool Activo
     );
 
+//DTO para mostrar los productos
   public record ProductListItemDto(
     int ProductId,
     string TipoProducto,
