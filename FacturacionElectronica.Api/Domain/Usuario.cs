926557d88fbf8bd323539481997522dc9ac96f10
Maybelline Navarro <maybenavarro20@gmail.com>
2025-11-09 22:45:57 -0500

    Contrasenia Hasheada

--- FacturacionElectronica.Api/Domain/Usuario.cs
+++ FacturacionElectronica.Api/Domain/Usuario.cs
@@ -5,7 +5,7 @@ namespace FacturacionElectronica.Api.Domain
     public int Id { get; set; }
     public string Nombre { get; set; } = default!;
     public string Apellido { get; set; } = default!;
-    public string ContrasenaHash { get; set; } = default!;
+    public string ContrasenaHash { get; set; } = default!; //contraseña hasheada
     public string Rol { get; set; } = default!;    // "admin" | "empleado"
     public string Correo { get; set; } = default!;
     public string Estado { get; set; } = "activo"; // "activo" | "inactivo"
